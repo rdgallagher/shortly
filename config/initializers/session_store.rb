7287646f6a803f9359e04c91027ef2a2178bbde7
Robert Gallagher <robert@wellmadeworld.com>
2012-07-08 16:23:47 -0400

    Renaming shrtly to shortly

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Shortly::Application.config.session_store :cookie_store, key: '_shrtly_session'
+Shortly::Application.config.session_store :cookie_store, key: '_shortly_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
